d6313c46072db6b4b5a7a7c8e38e08232eb7d169
Victória Eduarda <vitoriaeduarda853@gmail.com>
2025-12-29 23:29:42 +0000

    commit super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,15 +6,16 @@ int main(){
 setlocale(LC_ALL, "");
 
 //declaração de variáveis
-char cidade1 [20];
-char estado1 [20];
-char codigo1 [4];
-float populacao1;
-float km1;
-float pib1;
-int turistas1;
-float densidade1;
-float percapita1;
+char cidade1 [20], cidade2 [20];
+char estado1 [20], estado2 [20];
+char codigo1 [4], codigo2 [4];
+unsigned long int populacao1, populacao2;
+float km1, km2;
+float pib1, pib2;
+int turistas1, turistas2;
+float densidade1, densidade2;
+float percapita1, percapita2;
+double superpoder1, superpoder2;
 
 //entrada de dados da primeira carta
 printf("Digite a cidade da carta:  \n");
@@ -27,7 +28,7 @@ printf("Digite o código da carta usando a inicial do estado e dois números:  \
 scanf("%3s", codigo1);
 
 printf("Digite a população da cidade:  \n");
-scanf("%f", &populacao1);
+scanf("%u", &populacao1);
 
 printf("Digite quantos km tem a cidade:  \n");
 scanf("%f", &km1);
@@ -40,18 +41,6 @@ scanf ("%d", &turistas1);
 //linha em branco antes da exibição da carta
 printf("\n");
 
-//variáveis da segunda carta
-char cidade2 [20];
-char estado2 [20];
-char codigo2 [4];
-float populacao2;
-float km2;
-float pib2;
-int turistas2;
-float densidade2;
-float percapita2;
-
-printf("\n");
 //entrada de dados da segunda carta
 printf("Digite a cidade da segunda carta:  \n");
 scanf("%s", cidade2);
@@ -63,7 +52,7 @@ printf("Digite o código da segunda carta usando a inicial do estado e dois núm
 scanf("%4s", codigo2);
 
 printf("Digite a população da segunda cidade:  \n");
-scanf("%f", &populacao2);
+scanf("%u", &populacao2);
 
 printf("Digite quantos km tem a segunda cidade:  \n");
 scanf("%f", &km2);
@@ -76,21 +65,28 @@ scanf ("%d", &turistas2);
 //linha em branco antes da segunda carta
 printf("\n");
 
-//calculo matematico
+//cálculo matemático sobre densidade e per capita
 densidade1 = (float) populacao1 / km1;
 percapita1 = (float) pib1 / populacao1;
 densidade2 = (float) populacao2 / km2;
 percapita2 = (float) pib2 / populacao2;
 
+//cálculo matemático para Super Poder
+superpoder1 = populacao1 + km1 + pib1 + turistas1 + percapita1 + (1.0 / densidade1);
+superpoder2 = populacao2 + km2 + pib2 + turistas2 + percapita2 + (1.0 / densidade2);
+
 //exibição da primeira carta
 printf("Carta 1: \nCidade: %s \nEstado: %s \nCódigo: %s \n", cidade1, estado1, codigo1); 
-printf("População: %.0f \nÁrea: %.2f km²\nPIB: %.2f bilhões de reais \nNúmero de pontos turísticos: %d\n",populacao1, km1, pib1, turistas1);
-printf("Densidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais", densidade1, percapita1);
+printf("População: %.0u \nÁrea: %.2f km²\nPIB: %.2f bilhões de reais \nNúmero de pontos turísticos: %d\n",populacao1, km1, pib1, turistas1);
 //espaço visual para a segunda carta
+printf("Densidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\nSuper Poder: %2.lf", densidade1, percapita1, superpoder1);
 printf("\n\n");
 printf("Carta 2: \nCidade: %s \nEstado: %s \nCódigo: %s \n", cidade2, estado2, codigo2);
-printf("População: %.0f \nÁrea: %.2f km²\nPIB: %.2f bilhões de reais \nNúmero de pontos turísticos: %d\n",populacao2, km2, pib2, turistas2);
-printf("Densidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais", densidade2, percapita2);
+printf("População: %.0u \nÁrea: %.2f km²\nPIB: %.2f bilhões de reais \nNúmero de pontos turísticos: %d\n",populacao2, km2, pib2, turistas2);
+printf("Densidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\nSuper Poder: %2.lf", densidade2, percapita2, superpoder2);
+
+//comparação das cartas
+printf("População carta 1 > carta 2? %d\n", populacao1, populacao2);
 
 return 0;
 }
